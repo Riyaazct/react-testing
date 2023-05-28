2c1901bc5101768d0bef42ea5369827e6577d8b1
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-05-28 17:48:39 +0200

    video 9 2nd test

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -1,3 +1,23 @@
+/**
+ * Greet should render the text hello and if a name is passed into the component
+ * it should render hello followed by the name
+ */
+
+import { render, screen } from "@testing-library/react";
+import { Greet } from "./Greet";
+
+test("greet renders correctly", () => {
+  render(<Greet />);
+  const textElement = screen.getByText("Hello");
+  expect(textElement).toBeInTheDocument();
+});
+
+/**
+ * FIRST WRITTEN TEST
+ * VIDEO #8 IN SERIES
+ */
+
+/*
 import { render, screen } from "@testing-library/react";
 import { Greet } from "./Greet";
 
@@ -6,3 +26,4 @@ test("Greet renders correctly", () => {
   const textElement = screen.getByText(/hello/i);
   expect(textElement).toBeInTheDocument();
 });
+*/
