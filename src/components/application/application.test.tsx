15bb342be8189ed3261c65349d982f2ac5339983
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-05-29 23:06:23 +0200

    testing for more than one of the same or similar elements in the dom

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -7,8 +7,16 @@ describe("Application", () => {
     render(<Application />);
 
     // TEXT INPUT ELEMENT
-    const nameElement = screen.getByRole("textbox");
+    const nameElement = screen.getByRole("textbox", {
+      name: "Name", // if there is more than one input element, you access the label associated with the element and use the text content (in this case) of the label element
+    });
     expect(nameElement).toBeInTheDocument();
+
+    // TEXT AREA ELEMENT
+    const bioElement = screen.getByRole("textbox", {
+      name: "Bio",
+    });
+    expect(bioElement).toBeInTheDocument();
 
     // SELECT ELEMENT
     const jobLocationElement = screen.getByRole("combobox");
