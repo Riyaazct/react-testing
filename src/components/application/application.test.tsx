be6453c05760065a4d9747a473be3718f8b6013b
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-05-29 22:32:32 +0200

    end of lesson 18

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -3,16 +3,22 @@ import { Application } from "./Application";
 
 describe("Application", () => {
   test("renders correctly", () => {
+    // RENDER THE COMPONENT TO THE DOM
     render(<Application />);
+
+    // TEXT INPUT ELEMENT
     const nameElement = screen.getByRole("textbox");
     expect(nameElement).toBeInTheDocument();
 
+    // SELECT ELEMENT
     const jobLocationElement = screen.getByRole("combobox");
     expect(jobLocationElement).toBeInTheDocument();
 
+    // CHECKBOX ELEMENT
     const termsAndConditionsElement = screen.getByRole("checkbox");
     expect(termsAndConditionsElement).toBeInTheDocument();
 
+    // BUTTON ELEMENT
     const buttonElement = screen.getByRole("button");
     expect(buttonElement).toBeInTheDocument();
   });
