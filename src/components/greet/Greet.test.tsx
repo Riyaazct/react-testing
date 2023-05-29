9c7538cee0dd5afa5d98e6e2e411d1bc667b355c
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-05-29 21:47:21 +0200

    react testing library

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -1,35 +1,23 @@
-/**
+import { render, screen } from "@testing-library/react";
+import { Greet } from "./Greet";
+
+/*
  * Greet should render the text hello and if a name is passed into the component
  * it should render hello followed by the name
  */
 
-import { render, screen } from "@testing-library/react";
-import { Greet } from "./Greet";
+/* test suite */
+describe("Greet", () => {
+  /* Tests */
+  test("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
 
-test("greet renders correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText("Hello");
-  expect(textElement).toBeInTheDocument();
+  test("renders a name", () => {
+    render(<Greet name="Riyaaz" />);
+    const textElement = screen.getByText("Hello Riyaaz");
+    expect(textElement).toBeInTheDocument();
+  });
 });
-
-test("greet renders with a name", () => {
-  render(<Greet name="Riyaaz" />);
-  const textElement = screen.getByText("Hello Riyaaz");
-  expect(textElement).toBeInTheDocument();
-});
-
-/**
- * FIRST WRITTEN TEST
- * VIDEO #8 IN SERIES
- */
-
-/*
-import { render, screen } from "@testing-library/react";
-import { Greet } from "./Greet";
-
-test("Greet renders correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText(/hello/i);
-  expect(textElement).toBeInTheDocument();
-});
-*/
